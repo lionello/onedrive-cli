c52f4ab2c75fe77c5a6d537c7cc75d2dd96d7d67
Lionello Lunesu <lio+git@lunesu.com>
2020-03-29 15:17:51 +0800

    Using latest pnpm

--- shell.nix
+++ shell.nix
@@ -1,15 +1,7 @@
 with import <nixpkgs> {};
-let
-  pnpmPkg = nodePackages_10_x.pnpm;
-  # npm ERR! Unsupported URL Type "link:": link:../../privatePackages/assert-project
-  pnpm = (pnpmPkg.override (old: {
-      preRebuild = ''
-        sed -i 's|link:|file:|' package.json
-      '';
-  }));
-in mkShell {
+mkShell {
   buildInputs = [
     nodejs-10_x
-    pnpm
+    nodePackages_10_x.pnpm
   ];
 }
