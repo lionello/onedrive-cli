78a7ea9852d3cab626f342c86bd2e067a4b3d8cd
Lionello Lunesu <lio+git@lunesu.com>
2020-11-10 06:56:37 -0800

    Stick to single quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -238,7 +238,10 @@ module.exports = {
         "prefer-spread": "error",
         "prefer-template": "off",
         "quote-props": "off",
-        "quotes": "off",
+        "quotes": [
+            "error",
+            "single"
+        ],
         "radix": "error",
         "require-await": "error",
         "require-jsdoc": "off",
