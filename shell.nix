467bc42f6d14e77daaa1514111c641fa11b18461
Lionello Lunesu <lio+git@lunesu.com>
2024-02-20 11:25:54 -0800

    update dependencies

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 mkShell {
   buildInputs = [
-    nodejs-14_x
+    nodejs-18_x
     nodePackages.pnpm
     ruby.gems.redcarpet # for readme generation
     nodePackages.node2nix
